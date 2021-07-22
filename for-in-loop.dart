26780bd38abf15ff391acd3ccc84b6ddeee830a1
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-22 14:10:05 +0545

    Program to print given data line by line.

---
+++ for-in-loop.dart
@@ -0,0 +1,17 @@
+// [Ram, Shyam, Hari, Gita, Sita, Chutiya, Iluminati, Putin];
+
+void main() {
+  var data = [
+    'Ram',
+    'Shyam',
+    'Hari',
+    'Gita',
+    'Sita',
+    'Chutiya',
+    'Iluminati',
+    'Putin'
+  ];
+  for (String finaldata in data) {
+    print(finaldata);
+  }
+}
