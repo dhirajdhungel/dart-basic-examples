172d3af6a026a110a1d4c7303e38bd3ff8f38828
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-22 14:39:37 +0545

    Returns a list of the UTF-16 code units of a given string.

--- string-units.dart
+++ string-units.dart
@@ -1,6 +1,6 @@
 // Returns a list of the UTF-16 code units of a given string.
 
 void main() {
-  String sentance = "Hello World, I'm learning cool language called Dart.";
+  String sentance = "Hello World, I'm learning cool language called Dart. ";
   print(sentance.codeUnits);
 }
