6d817d331dc61aa2c3e6a45172a0f072f49147a7
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-22 14:29:15 +0545

    Automation on Printing numbers using for-loop

--- for-loop.dart
+++ for-loop.dart
@@ -1,8 +1,8 @@
 void main() {
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 3; i++) {
     print("\n-----------Part ${i + 1} -----------");
     print("Outerloop: $i");
-    for (var j = 0; j < 10; j++) {
+    for (var j = 0; j < 5; j++) {
       print("\tInnerloop: $j");
     }
   }
