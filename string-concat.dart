51ab4b54b8a46b848ae59af6f9914717bd64a208
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-22 14:34:56 +0545

    Join two strings

--- string-concat.dart
+++ string-concat.dart
@@ -2,5 +2,6 @@ void main() {
   String word1 = "Mis";
   String word2 = "Fit";
   // String concat = word1 + word2;
+  //above works same too, just add $concat in print statement.
   print("$word1 + $word2 = ${word1 + word2}");
 }
