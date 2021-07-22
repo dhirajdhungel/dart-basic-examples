11ee933c236525e5c2c3b51216c8ce7d5fa2e807
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-22 14:26:01 +0545

    boolean problem and guide on Console input

--- boolean.dart
+++ boolean.dart
@@ -1,15 +1,15 @@
 import 'dart:io'; //importing dart library
 
 void main() {
-  bool value;
+  bool bvalue;
   print(
       "The following operation checks \n if first Number is smaller than second number.\n \t ie. Number1 < Number2");
   int? num1 =
       int.parse(stdin.readLineSync()!); // '?' and '!' are for Null safety.
   int? num2 =
       int.parse(stdin.readLineSync()!); // '?' and '!' are for Null safety.
-  value = num1 < num2;
-  print("The above statement is $value ");
+  bvalue = num1 < num2;
+  print("The Above Statement is $bvalue.");
 }
 
 // Below is an example for console input
