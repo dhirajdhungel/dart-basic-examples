453baec12ef9377234f9cc93be8fe87d3256fa4e
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-23 10:32:51 +0545

    Simple while loop and Standard input

--- while-loop.dart
+++ while-loop.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   int factorial = 6;
-  stdout.write("Enter a number to calculate factorial to which\t:"); //
+  stdout.write("Enter a number to calculate factorial to which upto\t:"); //
   int num = int.parse(stdin.readLineSync()!);
   while (num >= 1) {
     factorial = num * factorial;
