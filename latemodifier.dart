cf5910c6b8fc4f156b8dba505c6527dd9eca41b2
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-23 13:54:43 +0545

    Lazy loading late modifier

---
+++ latemodifier.dart
@@ -0,0 +1,12 @@
+late String name = testfunction();
+
+String testfunction() {
+  print("Test Function is called sucessfully");
+  return "This text is returned";
+}
+
+void main() {
+  // name = "Howard Pumpkin";
+  print(name);
+  // name;
+}
