10932726ab93d7bb636952ee687b7c95533cde96
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-23 10:31:03 +0545

    Simple hile loop and Standard input

--- while-loop.dart
+++ while-loop.dart
@@ -1,10 +1,9 @@
-//problem
 import 'dart:io';
 
 void main() {
-  int factorial = 0;
-  stdout.write("Enter a number to calculate factorial to which\t:");
-  int? num = int.parse(stdin.readLineSync()!);
+  int factorial = 6;
+  stdout.write("Enter a number to calculate factorial to which\t:"); //
+  int num = int.parse(stdin.readLineSync()!);
   while (num >= 1) {
     factorial = num * factorial;
     num--;
