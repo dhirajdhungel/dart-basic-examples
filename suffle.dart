77632358af5e803feba18de2dffc4d2533a3767f
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-25 18:23:28 +0545

    Suffle list and shows rock-paper-scissor in random

--- suffle.dart
+++ suffle.dart
@@ -4,6 +4,6 @@ void main() {
   var list = ['ROCK', 'PAPER', 'SCISSOR'];
   var randomItem = (list.toList()..shuffle())
       .first; //this simply shuffles the item of list and execute the first one from shuffeled list.
-  print("Rock-----Paper----Scissor");
+  print("Rock------Paper-----Scissor");
   new Timer(const Duration(seconds: 2), () => print("\n\t\t $randomItem \n"));
 }
