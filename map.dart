ef92c863a48dc33998ab58c9323f071a39419f4f
dhirajdhungel <dhirazdhungel@gmail.com>
2021-07-25 18:24:54 +0545

    Storing user data in collection using map

--- map.dart
+++ map.dart
@@ -3,13 +3,9 @@ void main() {
     'USERNAME': 'PASSWORD',
     'Haramkhor': 'harami!@##',
     'chutiya': 'chutiyappa123',
-    'bhagbsdk': 'bsdk123',
+    'bhagbsdk': 'bsdK123',
   };
   user_data.forEach((key, value) {
     print("$key - $value");
   });
-  // print("\n\n");
-  // for (String data in user_data) {
-  //   print(data);
-  // }
 }
